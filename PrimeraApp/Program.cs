a1bcff36ecc691ed8902ce46038df73d382798ee
VictoriaRipa <112644052+VictoriaRipa@users.noreply.github.com>
2023-10-27 10:52:54 -0300

    Ejemplos

---
+++ PrimeraApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declaracion de dos variables enteras.
+        //   int numero1 = 5;
+        //   int numero2 = 3;
+        //int suma = numero1 + numero2;
+        //int producto = numero1 * numero2;
+        //Console.WriteLine("La suma es: " + suma);
+        //Console.WriteLine("EL producto es: " + producto);
+        //Console.ReadLine();
+
+        //int valor1 = 9;
+        //int valor2 = 2;
+        //valor1 %= valor2;
+        //Console.WriteLine("Resultado {0}", valor1);
+        //Console.ReadLine();
+
+        //int edad = 18;
+        //if (edad >= 18)
+        //{
+        //    Console.WriteLine("Eres mayor de edad");
+
+        //}
+        //else {
+        //    Console.WriteLine("Eres menor de edad");
+        //}
+
+        //    for (int i=1; i<=10;i++) {
+        //        Console.WriteLine("Numero" + i);
+        //    }
+        //    Console.ReadLine();
+
+
+        //}
+        //int contador = 1; // Inicializamos el contador en 1
+        //while (contador<=5) // mientras el contador sea menor o igual a 5
+        //{
+        //    Console.WriteLine("Numero: " + contador); // imprime el valor del contador
+        //    contador++; //incrementa el contador en 1 en cada iteracion
+        //}
+        //Console.ReadLine();
+
+    //    Console.Write("Ingrese un numero del 1 al 4");
+    //    int numero = int.Parse(Console.ReadLine());
+    //    switch (numero)
+    //    {
+    //        case 1:
+    //    Console.WriteLine("Has elegido el numero 1");
+    //    break;
+    //        case 2: 
+    //        Console.WriteLine("Has elegido el numero 2");
+    //    break;
+
+    //         case 3: 
+    //        Console.WriteLine("Has elegido el numero 3");
+    //    break;
+    //         case 4: 
+    //        Console.WriteLine("Has elegido el numero 4");
+    //    break;
+    //    default:
+    //        Console.WriteLine("Numero no valido, ingrese un numero del 1 al 4");
+    //    break;
+    //}
+    }
+
+    }
